ebf3107d1caedb33e4fa1e75768e344cb82052f0
Maurice <curtis.virges@gmail.com>
2023-09-23 13:50:38 -0400

    incomplete

--- problems/13-sort.js
+++ problems/13-sort.js
@@ -22,8 +22,17 @@ sort([]); // []
 ***********************************************************************/
 
 function sort(nums, sorted = []) {
-    // Your code here 
-}
+   if (nums.length === 0) return sorted;
 
+    for(let i = 1; i < nums.length; i++){
+        let num = nums[i]
+        if(num < nums[0]) sorted.push(num)
+        else sorted.push(sort(nums[0], sorted))
+    }
+    return sorted
+}
+console.log(sort([4,1,6,3,1,7]))
+console.log(sort([]))
+console.log(sort([0, 1, -3]))
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = sort;
